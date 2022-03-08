3dbe0b6afa158f205fe5d0d02ddd0f67bac0103f
Siddhesh Zantye <28945611+FusionSid@users.noreply.github.com>
2022-03-08 09:48:09 +0000

    Working + Host on replit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, session
 import requests
+from threading import Thread
 from config import *
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 class User():
-    def __init__(self, username, discriminator, _id, avatar_hash):
+    def __init__(self, username=None, discriminator=None, _id=None, avatar_hash=None):
         self.username = username
         self.discriminator = discriminator
         self.id = _id
@@ -20,17 +21,21 @@ def home():
 
     if not access_token:
         return render_template("index.html")
-    
-    user_json  = requests.get("https://discordapp.com/api/users/@me", headers={"Authorization": "Bearer "+ access_token}).json()
 
-    user = User(
-        user_json["username"],
-        user_json["discriminator"],
-        user_json["id"],
-        user_json["avatar"]
-    )
-
-    return render_template("index.html", user=user)
+    try:
+      user_json  = requests.get("https://discord.com/api/users/@me", headers={"Authorization": f"Bearer {access_token}"})
+      user_json = user_json.json()
+  
+      user = User(
+          user_json["username"],
+          user_json["discriminator"],
+          user_json["id"],
+          user_json["avatar"]
+      )
+  
+      return render_template("index.html", user=user)
+    except:
+      return render_template("index.html")
 
 @app.route("/logout")
 def logout():
@@ -45,19 +50,20 @@ def joinserver(userid : int):
         "access_token" : access_token
     }
     headers = {
-        "authorization": f"Bot {BOT_TOKEN}",
+        "Authorization": f"Bot {BOT_TOKEN}",
         "content-type": "application/json"
     }
 
     response_json  = requests.put(f"https://discordapp.com/api/guilds/763348615233667082/members/{userid}", json=data, headers=headers)
-    
+    print(response_json.status_code)
+
     return redirect("/")
 
 @app.route("/login")
 def login():
     return redirect(OAUTH_URL)
     
-@app.route("/oauth/callback")
+@app.route("/oauth/callback", methods=["GET", "POST"])
 def oauth_callback():
     code = request.args["code"]
 
@@ -69,14 +75,14 @@ def oauth_callback():
         'redirect_uri': REDIRECT_URI
     }
     headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'Authorization' : f"Bot {BOT_TOKEN}"
     }
     get_access_token = requests.post('https://discord.com/api/oauth2/token', data=data, headers=headers).json()
-    
+
     session["access_token"] = get_access_token["access_token"]
 
     return redirect("/")
 
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=PORT)
+app.run(host="0.0.0.0", port=PORT)
